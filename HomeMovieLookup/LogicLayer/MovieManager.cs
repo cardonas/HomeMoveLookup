2e226db340aeb3dc0806172096c05d1ed8cff970
Steven Cardona <cardona.sj@gmail.com>
2019-11-15 11:04:46 -0600

    commit 2

---
+++ HomeMovieLookup/LogicLayer/MovieManager.cs
@@ -0,0 +1,44 @@
+﻿using MovieDataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace MovieControlLayer
+{
+    public class MovieManager
+    {
+        List<Movie> _movieList;
+        public MovieManager()
+        {
+            try
+            {
+                 _movieList = MovieDataAccessor.RetrieveMovieListAsync().Result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public List<Movie> MovieLIst
+        {
+            get { return _movieList;}
+        }
+
+        public async Task<List<Movie>> AddMovieAsync(Movie movie)
+        {
+            try
+            {
+                if (MovieDataAccessor.AddMovie(movie))
+                {
+                    _movieList = await MovieDataAccessor.RetrieveMovieListAsync();
+                }
+                return _movieList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
